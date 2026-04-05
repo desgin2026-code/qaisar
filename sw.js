0e0af633ff4ae9bfd923b017d8493aef97f47eb2
desgin2026-code <desgin2026@gmail.com>
2026-04-05 23:08:19 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-// ============================================================
-// Service Worker — سنتر القيصر
-// ارفع الملف ده على GitHub في نفس مجلد index.html
-// المسار: /qaisar/sw.js
-// ============================================================
-
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
